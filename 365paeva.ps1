0be9303064bd43c756920c6b93bc45fbd8d14607
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-02-16 11:42:26 +0200

    Add files via upload

---
+++ 365paeva.ps1
@@ -0,0 +1,38 @@
+﻿#martin pettai
+#it21
+#365 päeva
+#16.02.22
+cls
+
+#kasutajate viimati sisselogimisajad
+
+$OU="OU=KASUTAJAD,DC=PETTAI,DC=LOCAL"
+$ADkasutajad=Get-ADUser -filter * -Properties distinguishedname, LastLogon | Select DistinguishedName, Samaccountname, @{name="lastlogon";expression={[datetime]::FromFileTime($_.'lastlogon')}}
+
+#write-output $adkasutajad
+
+foreach($kasutaja in $ADkasutajad){
+    $knimi=$kasutaja.samaccountname
+    $login=$kasutaja.lastlogon
+    $path=$kasutaja.DistinguishedName
+    
+    
+
+    $kuupaev=Get-Date 
+    $vahe=(New-TimeSpan -Start $login -End $kuupaev).Days
+    
+
+    
+
+    if ($vahe -gt 365){
+        Move-ADObject -Identity $path -TargetPath "OU=EndisedTootajad,DC=PETTAI,DC=LOCAL"
+        Disable-ADAccount -Identity $knimi
+        }
+            else
+        {
+            Write-Host "kasutaja $knimi autentis ennast viimati $vahe päeva tagasi"
+        }
+        
+       
+
+}
---
+++ kasutajatelisamineousse.ps1
@@ -0,0 +1,77 @@
+﻿#Martin Pettai
+#hkhk
+#07.02.22
+#it21
+#ad
+cls
+
+
+$dir = "C:\POWERSHELL"
+
+$kasutajad = Import-Csv $dir\oigedkasutajad.csv
+#$domeen="@pettai.local"
+$parool="Par00lA"
+
+$logheader = @("Kuupäev ja kellaaeg      info")
+$logheader | set-content $dir\kasutajalog.log
+
+
+foreach($kasutaja in $kasutajad){
+    
+    $enimi = $kasutaja.eesnimi
+    $pnimi = $kasutaja.perekonnanimi
+    $osakond = $kasutaja.osakond
+    $OU="OU=$osakond,OU=KASUTAJAD,DC=PETTAI,DC=LOCAL"
+    $teekond="OU=KASUTAJAD,DC=PETTAI,DC=LOCAL"
+
+    $knimi = $enimi.ToLower()+"."+$pnimi.toLower()
+    
+    if (Get-ADOrganizationalUnit -filter "distinguishedname -eq '$OU'"){
+        Write-Warning "$OU on juba olemas"
+        $detailid = (Get-Date -UFormat "%d/%m/%y %T%t")  + "Kontrolliti kas OU on olemas: JAH"
+        add-content $dir\kasutajalog.log $detailid       
+    }
+    else
+    {
+        New-ADOrganizationalUnit -Name $osakond -Path $teekond 
+        Write-Host "loodi uus ou $OU"
+        $detailid = (Get-Date -UFormat "%d/%m/%y %T%t")  + "Loodi uus OU"
+        add-content $dir\kasutajalog.log $detailid 
+    }
+
+    if(Get-ADUser -F { SamAccountname -eq $knimi }){
+    Write-Warning "kasutaja nimega $knimi on juba olemas" 
+    $detailid = (Get-Date -UFormat "%d/%m/%y %T%t")  + "Kasutaja nimi $knimi oli olemas"
+    add-content $dir\kasutajalog.log $detailid        
+            
+        }
+        else
+        {
+           
+            New-ADUser `
+                -SamAccountName $knimi `
+                -name "$enimi $pnimi" `
+                -GivenName $enimi `
+                -Surname $pnimi `
+                -Enabled $true `
+                -Path $OU `
+                -Department $osakond `
+                -AccountPassword (ConvertTo-SecureString $parool -AsPlainText -Force) -ChangePasswordAtLogon $true
+                
+            Write-Output "luua uus kasutaja nimega $knimi"
+            $detailid = (Get-Date -UFormat "%d/%m/%y %T%t")  + "Loodi kasutaja $knimi"
+            add-content $dir\kasutajalog.log $detailid     
+
+        }
+         
+    Write-Output "$enimi $pnimi kasutaja nimi on $knimi ja osakonnaks $osakond"
+
+
+    
+    
+    
+
+
+}
+
+import-csv $dir\kasutajalog.log
